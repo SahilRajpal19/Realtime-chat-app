f4ffe4a76dc30af6a8f9d26a98128f9649ce3945
SahilRajpal19 <sahilrajpal40@gmail.com>
2025-05-17 14:10:36 -0400

    Error Fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from"dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from path;
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
